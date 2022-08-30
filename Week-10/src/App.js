39f022e34ea767846688c77c216b59451445f5c3
aditya-vetale <adityavetale7028@gmail.com>
2022-08-30 17:03:39 +0530

    Added URL shorten API functionality and made some changes in front-end

--- Week-10/src/App.js
+++ Week-10/src/App.js
@@ -1,5 +1,9 @@
-import "./App.css";
-
-function App() {}
-
-export default App;
+// import "./App.css";
+const axios = require("axios");
+let url =
+  "https://openbase.com/categories/js/best-javascript-url-shortener-api-libraries";
+async function handler() {
+  const res = await axios(`https://api.shrtco.de/v2/shorten?url=${url}`);
+  console.log(res.data.result.full_short_link);
+}
+handler();
--- Week-10/src/footer/footer.js
+++ Week-10/src/footer/footer.js
@@ -1,16 +1,43 @@
 import "./style.css";
-import React from "react";
+import React, { useState } from "react";
+const axios = require("axios");
 export default function Footer() {
+  const [url, setURL] = useState(null);
+  const [shortURL, setShortURL] = useState(null);
+  function MakeURLshort() {
+    async function handler() {
+      const res = await axios(`https://api.shrtco.de/v2/shorten?url=${url}`);
+      setShortURL(res.data.result.full_short_link);
+      setURL("");
+    }
+    handler();
+  }
+
+  console.log({ url });
   return (
     <div className="Footer">
       <div className="Input">
         <input
           type="text"
           id="input-url"
-          placeholder="Shorten your URL"
+          placeholder="Paste your link ......"
+          value={url}
+          onChange={(e) => setURL(e.target.value)}
         ></input>
-        <button className="shorten-button">Shorten</button>
+        <button className="shorten-button" onClick={() => MakeURLshort()}>
+          Shorten
+        </button>
       </div>
+      {shortURL ? (
+        <div id="clear">
+          <text id="output">{shortURL}</text>
+          <button id="output-button" onClick={() => setShortURL(null)}>
+            Clear
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
